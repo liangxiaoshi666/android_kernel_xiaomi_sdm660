aa31f83276d7fa465867a1dae7ca224e5dc7b3ea
liangxiaoshi666 <3059897901@qq.com>
2026-02-13 12:14:01 +0800

    add Kernelsu

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Feb 13 12:07:22 CST 2026"
+#define LINUX_COMPILE_BY "liang"
+#define LINUX_COMPILE_HOST "DESKTOP-PL19A61"
+#define LINUX_COMPILER "Debian clang version 19.1.7 (3+b1)"
